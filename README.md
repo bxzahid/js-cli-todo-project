bca821d4c6aa8e7fd953d1f395d25894176223e5
bxzahid <bxzahid@gmail.com>
2021-02-16 04:41:53 +0600

    feat(command): added command

---
+++ README.md
@@ -0,0 +1,12 @@
+## JS CLI Todo Project
+
+### Setup Process
+```bash
+git clone https://github.com/bxzahid/js-cli-todo-project.git
+cd js-cli-todo-project
+```
+
+Install all the dependencies
+```bash
+npm install / yarn
+```
---
+++ src/command.js
@@ -0,0 +1,15 @@
+const CREATE = "create";
+const UPDATE = "update";
+const REMOVE = "remove";
+const SEARCH = "search";
+const DONE = "done";
+const LIST = "list";
+
+module.exports = {
+	CREATE,
+	UPDATE,
+	REMOVE,
+	SEARCH,
+	DONE,
+	LIST,
+};
